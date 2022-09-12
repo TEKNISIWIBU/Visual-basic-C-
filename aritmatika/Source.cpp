9b83eb6f5ffa98c34fc5d6b315b4394ec566aa52
TEKNISIWIBU <101981020+TEKNISIWIBU@users.noreply.github.com>
2022-09-12 20:02:18 +0700

    aritmatika

---
+++ aritmatika/Source.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+	int a = 6;
+	int b = 5;
+	int hasil;
+
+	//operator aritmatika +,-,*,/,%
+	//penjumlahan
+    hasil = a + b;
+	cout << a <<"+"<< b << "="<< hasil<< endl;
+	//pengurangan
+	hasil = a - b;
+	cout << a << "-" << b << "=" << hasil << endl;
+	//perkalian
+	hasil = a * b;
+	cout << a << "x" << b << "=" << hasil << endl;
+      cin.get();
+	return 0;
+}
